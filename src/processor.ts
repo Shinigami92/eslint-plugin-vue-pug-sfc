3f74e7f7e735bd201938b9f3f892840c25aa035d
Shinigami92 <chrissi92@hotmail.de>
2021-09-16 14:02:09 +0200

    Do not run preprocess/postprocess

--- src/processor.ts
+++ src/processor.ts
@@ -1,14 +1,12 @@
 import type { Linter } from 'eslint';
 
 export default {
-  preprocess(text, filename) {
-    console.log('preprocess', text, filename);
-
-    return [text];
-  },
-  postprocess(messages, filename) {
-    console.log('postprocess', messages, filename);
-
-    return [];
-  }
+  // preprocess(text, filename) {
+  //   // console.log('preprocess', text, filename);
+  //   return [text];
+  // },
+  // postprocess(messages, filename) {
+  //   // console.log('postprocess', messages, filename);
+  //   return [];
+  // }
 } as Linter.Processor;
