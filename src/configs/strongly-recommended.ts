1ff1884615485f5810aca9d9e8e80ce2c8416550
Shinigami92 <chrissi92@hotmail.de>
2021-09-22 21:32:25 +0200

    Migrate `attribute-hyphenation`

--- src/configs/strongly-recommended.ts
+++ src/configs/strongly-recommended.ts
@@ -4,6 +4,7 @@ export = {
     {
       files: ['*.vue'],
       rules: {
+        'vue-pug-sfc/attribute-hyphenation': 'warn',
         'vue-pug-sfc/component-name-in-template-casing': 'warn'
       }
     }
---
+++ src/configs/vue3-recommended.ts
@@ -0,0 +1,11 @@
+export = {
+  extends: require.resolve('./vue3-strongly-recommended'),
+  overrides: [
+    {
+      files: ['*.vue'],
+      rules: {
+        'vue-pug-sfc/this-in-template': 'warn'
+      }
+    }
+  ]
+};
