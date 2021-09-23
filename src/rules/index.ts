4bf56691a5fa491799c4d7051277e318d3a2c6ff
Shinigami92 <chrissi92@hotmail.de>
2021-09-23 18:06:01 +0200

    Expose rule

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,10 +1,12 @@
 import type { Rule } from 'eslint';
 import attributeHyphenation from './attribute-hyphenation';
 import componentNameInTemplateCasing from './component-name-in-template-casing';
+import noDuplicateAttributes from './no-duplicate-attributes';
 import thisInTemplate from './this-in-template';
 
 export default {
   'attribute-hyphenation': attributeHyphenation,
   'component-name-in-template-casing': componentNameInTemplateCasing,
+  'no-duplicate-attributes': noDuplicateAttributes,
   'this-in-template': thisInTemplate
 } as Record<string, Rule.RuleModule>;
