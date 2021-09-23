4fabfdf46f479fb782d3699c227474d137f202c7
Shinigami <chrissi92@hotmail.de>
2021-09-23 15:53:59 +0200

    Improve token range calculation (#114)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -119,6 +119,24 @@ export function tokenLength(token: lex.Token, previousToken?: lex.Token): number
     return length + (diff - 1);
   }
 
+  if (
+    token.type === 'end-attributes' &&
+    previousToken?.type === 'attribute' &&
+    // Detect brace on new line wrapping after last attribute
+    token.loc.start.line - 1 === previousToken.loc.end.line
+  ) {
+    return 0;
+  }
+
+  if (
+    token.type === 'outdent' &&
+    previousToken &&
+    // Some outdents seems to be larger than one blank line
+    token.loc.start.line - 1 > previousToken.loc.end.line
+  ) {
+    return token.loc.end.column;
+  }
+
   if (token.loc.start.line === token.loc.end.line) {
     return token.loc.end.column - token.loc.start.column;
   }
