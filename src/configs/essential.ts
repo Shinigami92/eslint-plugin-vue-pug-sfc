82119953e531e1eeae5e19a4d36cf0ce4d900ed3
Shinigami92 <chrissi92@hotmail.de>
2021-10-02 15:17:30 +0200

    Migrate `require-component-is`

--- src/configs/essential.ts
+++ src/configs/essential.ts
@@ -2,6 +2,7 @@ export = {
   extends: require.resolve('./base'),
   rules: {
     'vue-pug-sfc/no-duplicate-attributes': 'error',
+    'vue-pug-sfc/require-component-is': 'error',
     'vue-pug-sfc/require-v-for-key': 'error'
   }
 };
