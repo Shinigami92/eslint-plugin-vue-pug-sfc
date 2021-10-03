eeb84120f78de50ebd3b26d3dce9f0f51bec6cf0
Shinigami <chrissi92@hotmail.de>
2021-10-03 18:54:52 +0200

    Migrate `no-dupe-v-else-if` (#118)
    Co-authored-by: ST-DDT <ST-DDT@gmx.de>

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,6 +1,7 @@
 import type { Rule } from 'eslint';
 import attributeHyphenation from './attribute-hyphenation';
 import componentNameInTemplateCasing from './component-name-in-template-casing';
+import noDupeVElseIf from './no-dupe-v-else-if';
 import noDuplicateAttributes from './no-duplicate-attributes';
 import requireComponentIs from './require-component-is';
 import requireVForKey from './require-v-for-key';
@@ -9,6 +10,7 @@ import thisInTemplate from './this-in-template';
 export default {
   'attribute-hyphenation': attributeHyphenation,
   'component-name-in-template-casing': componentNameInTemplateCasing,
+  'no-dupe-v-else-if': noDupeVElseIf,
   'no-duplicate-attributes': noDuplicateAttributes,
   'require-component-is': requireComponentIs,
   'require-v-for-key': requireVForKey,
---
+++ src/rules/no-dupe-v-else-if.ts
@@ -0,0 +1,106 @@
+import type { Rule } from 'eslint';
+import * as lex from 'pug-lexer';
+import { checkIsVueFile, parsePugContent } from '../utils';
+
+function isSubset(operandsA: string | boolean, operandsB: string | boolean): boolean {
+  // TODO: Check conditions more deeply
+  return operandsA === operandsB;
+}
+
+export default {
+  meta: {
+    type: 'problem',
+    docs: {
+      description: 'disallow duplicate conditions in `v-if` / `v-else-if` chains',
+      categories: ['vue3-essential', 'essential'],
+      url: 'https://eslint.vuejs.org/rules/no-dupe-v-else-if.html'
+    },
+    fixable: undefined,
+    schema: [],
+    messages: {
+      unexpected:
+        'This branch can never execute. Its condition is a duplicate or covered by previous conditions in the `v-if` / `v-else-if` chain.'
+    }
+  },
+  create(context) {
+    if (!checkIsVueFile(context)) {
+      return {};
+    }
+
+    const { tokens } = parsePugContent(context);
+
+    if (tokens.length === 0) {
+      return {};
+    }
+
+    let indentLevel: number = 0;
+    const ifs: lex.AttributeToken[][] = [];
+    const currentTagHasIf: boolean[] = [];
+
+    for (let index: number = 0; index < tokens.length; index++) {
+      const token: lex.Token = tokens[index]!;
+
+      if (token.type === 'indent') {
+        indentLevel++;
+        continue;
+      }
+
+      if (token.type === 'outdent') {
+        delete ifs[indentLevel];
+        indentLevel--;
+        continue;
+      }
+
+      if (token.type === 'tag') {
+        if (!currentTagHasIf[indentLevel]) {
+          delete ifs[indentLevel];
+        }
+        currentTagHasIf[indentLevel] = false;
+        continue;
+      }
+
+      if (token.type === 'attribute') {
+        if (token.name === 'v-if') {
+          ifs[indentLevel] = [token];
+          currentTagHasIf[indentLevel] = true;
+        } else if (
+          token.name === 'v-else-if' &&
+          ((typeof token.val === 'string' && token.val) || typeof token.val !== 'string')
+        ) {
+          currentTagHasIf[indentLevel] = true;
+          if (!Array.isArray(ifs[indentLevel])) {
+            ifs[indentLevel] = [];
+          }
+
+          if (ifs[indentLevel]!.some((tok) => isSubset(tok.val, token.val))) {
+            const loc: lex.Loc = token.loc;
+
+            const columnStart: number = loc.start.column - 1 + 'v-else-if="'.length;
+            const columnEnd: number = columnStart - 1 + String(token.val).length - '"'.length;
+
+            context.report({
+              node: {} as unknown as Rule.Node,
+              loc: {
+                line: loc.start.line,
+                column: loc.start.column - 1,
+                start: {
+                  line: loc.start.line,
+                  column: columnStart
+                },
+                end: {
+                  line: loc.end.line,
+                  column: columnEnd
+                }
+              },
+              messageId: 'unexpected'
+            });
+          }
+
+          ifs[indentLevel]!.push(token);
+        }
+      }
+    }
+
+    return {};
+  }
+} as Rule.RuleModule;
