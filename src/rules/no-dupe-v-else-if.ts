e76027220f31736aea9655716e92adb2a7ccaf07
Shinigami92 <chrissi92@hotmail.de>
2021-10-03 17:28:44 +0200

    Check only on same indent level
    Co-authored-by: ST-DDT <ST-DDT@gmx.de>

--- src/rules/no-dupe-v-else-if.ts
+++ src/rules/no-dupe-v-else-if.ts
@@ -28,74 +28,57 @@ export default {
       return {};
     }
 
-    let lastTagTokenIndex: number | undefined;
-    let lastStartAttributesTokenIndex: number | undefined;
-    const memory: Array<{
-      tagTokenIndex: number;
-      startAttributesTokenIndex: number;
-      token: lex.AttributeToken;
-      index: number;
-    }> = [];
+    let indentLevel: number = 0;
+    const ifs: lex.AttributeToken[][] = [];
 
     for (let index: number = 0; index < tokens.length; index++) {
       const token: lex.Token = tokens[index]!;
 
-      if (token.type === 'tag') {
-        lastTagTokenIndex = index;
+      if (token.type === 'indent') {
+        indentLevel++;
         continue;
       }
 
-      if (token.type === 'start-attributes') {
-        lastStartAttributesTokenIndex = index;
+      if (token.type === 'outdent') {
+        delete ifs[indentLevel];
+        indentLevel--;
         continue;
       }
 
       if (token.type === 'attribute') {
-        if (token.name === 'v-else-if' || token.name === 'v-if') {
-          // We can safely use non-null assertions, cause the lexer would not detect attributes before the occurrence of `tag` and `start-attributes` tokens.
-          memory.push({
-            tagTokenIndex: lastTagTokenIndex!,
-            startAttributesTokenIndex: lastStartAttributesTokenIndex!,
-            token,
-            index
-            // Do we need to store a indent-level?
-            // Maybe we can use the `token.loc.start.column`
-          });
+        if (token.name === 'v-if') {
+          ifs[indentLevel] = [token];
+        } else if (token.name === 'v-else-if') {
+          if (!Array.isArray(ifs[indentLevel])) {
+            ifs[indentLevel] = [];
+          }
+
+          if (ifs[indentLevel]!.some((tok) => tok.val === token.val)) {
+            const loc: lex.Loc = token.loc;
+
+            const columnStart: number = loc.start.column - 1;
+            const columnEnd: number = columnStart + 'v-else-if'.length;
+
+            context.report({
+              node: {} as unknown as Rule.Node,
+              loc: {
+                line: loc.start.line,
+                column: loc.start.column - 1,
+                start: {
+                  line: loc.start.line,
+                  column: columnStart
+                },
+                end: {
+                  line: loc.end.line,
+                  column: columnEnd
+                }
+              },
+              messageId: 'unexpected'
+            });
+          }
+
+          ifs[indentLevel]!.push(token);
         }
-      }
-    }
-
-    for (const { token, index } of memory) {
-      if (
-        token.name === 'v-else-if' &&
-        // Potentially we could optimize this loop by breaking out with a classic for-loop.
-        memory.some(
-          (t) =>
-            // We only need to check tokens before the current token
-            t.index < index && t.token.val === token.val
-        )
-      ) {
-        const loc: lex.Loc = token.loc;
-
-        const columnStart: number = loc.start.column - 1;
-        const columnEnd: number = columnStart + 'v-else-if'.length;
-
-        context.report({
-          node: {} as unknown as Rule.Node,
-          loc: {
-            line: loc.start.line,
-            column: loc.start.column - 1,
-            start: {
-              line: loc.start.line,
-              column: columnStart
-            },
-            end: {
-              line: loc.end.line,
-              column: columnEnd
-            }
-          },
-          messageId: 'unexpected'
-        });
       }
     }
 
