4be5a7ebcc3ab76d74d40389c8f69ff1d0898732
Christopher Quadflieg <christopher.quadflieg@adsoul.com>
2021-10-05 11:46:23 +0200

    wip

--- src/rules/use-v-on-exact.ts
+++ src/rules/use-v-on-exact.ts
@@ -1,5 +1,5 @@
 import type { Rule } from 'eslint';
-import type { Loc, Token } from 'pug-lexer';
+import type { AttributeToken, Loc, Token } from 'pug-lexer';
 import { checkIsVueFile, parsePugContent } from '../utils';
 
 export default {
@@ -24,46 +24,78 @@ export default {
       return {};
     }
 
-    let currentClickAttributeTokenIndexes: number[] = [];
+    let eventAttributes: AttributeToken[] = [];
 
     for (let index: number = 0; index < tokens.length; index++) {
       const token: Token = tokens[index]!;
 
+      // Reset attributes cache
       if (token.type === 'start-attributes') {
-        currentClickAttributeTokenIndexes = [];
+        eventAttributes = [];
         continue;
       }
 
-      if (token.type === 'attribute') {
-        if (token.name.startsWith('v-on:click') || token.name.startsWith('@click')) {
-          currentClickAttributeTokenIndexes.push(index);
-        }
+      // Add only event attributes
+      if (token.type === 'attribute' && (token.name.startsWith('v-on:') || token.name.startsWith('@'))) {
+        eventAttributes.push(token);
+        continue;
+      }
 
-        if (currentClickAttributeTokenIndexes.length < 2) {
-          continue;
-        }
+      // Check if there are similar event attributes
+      if (token.type === 'end-attributes' && eventAttributes.length > 1) {
+        const eventAttributeMatches: RegExpExecArray[] = eventAttributes.map(
+          (attr) => /^(v-on:|@)(?<name>[\w[\]]+)\.?(?<modifiers>(.+))?/.exec(attr.name)!
+        );
+        // Start at index 1 / skip first element
+        for (let outerIndex: number = 1; outerIndex < eventAttributes.length; outerIndex++) {
+          const eventAttributeToken: AttributeToken = eventAttributes[outerIndex]!;
+          const { name: eventName, modifiers: eventModifiersGroup } = eventAttributeMatches[outerIndex]!.groups!;
 
-        const loc: Loc = token.loc;
-
-        const columnStart: number = loc.start.column - 1;
-        const columnEnd: number = loc.end.column - 1;
-
-        context.report({
-          node: {} as unknown as Rule.Node,
-          loc: {
-            line: loc.start.line,
-            column: loc.start.column - 1,
-            start: {
-              line: loc.start.line,
-              column: columnStart
-            },
-            end: {
-              line: loc.end.line,
-              column: columnEnd
+          for (let matchIndex: number = 0; matchIndex < eventAttributeMatches.length; matchIndex++) {
+            if (matchIndex === outerIndex) {
+              // Don't compare attribute with itself
+              continue;
             }
-          },
-          message: "Consider to use '.exact' modifier."
-        });
+
+            const { name: otherName, modifiers: otherModifiersGroup } = eventAttributeMatches[matchIndex]!.groups!;
+
+            if (eventName === otherName) {
+              const eventModifiers: string[] = eventModifiersGroup?.split('.') ?? [];
+
+              if (!eventModifiers.includes('exact')) {
+                const otherModifiers: string[] = otherModifiersGroup?.split('.') ?? [];
+                if (
+                  otherModifiers.length === 0 ||
+                  !eventModifiers.every((modifier) => otherModifiers.includes(modifier))
+                ) {
+                  continue;
+                }
+
+                const loc: Loc = eventAttributeToken.loc;
+
+                const columnStart: number = loc.start.column - 1;
+                const columnEnd: number = loc.end.column - 1;
+
+                context.report({
+                  node: {} as unknown as Rule.Node,
+                  loc: {
+                    line: loc.start.line,
+                    column: loc.start.column - 1,
+                    start: {
+                      line: loc.start.line,
+                      column: columnStart
+                    },
+                    end: {
+                      line: loc.end.line,
+                      column: columnEnd
+                    }
+                  },
+                  message: "Consider to use '.exact' modifier."
+                });
+              }
+            }
+          }
+        }
       }
     }
 
