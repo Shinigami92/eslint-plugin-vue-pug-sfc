3dd2691237bd0f23cabc95faded0db696cd81d70
Shinigami92 <chrissi92@hotmail.de>
2021-10-16 16:01:20 +0200

    Migrate `valid-v-if`

--- src/configs/essential.ts
+++ src/configs/essential.ts
@@ -8,6 +8,7 @@ export = {
     'vue-pug-sfc/no-use-v-if-with-v-for': 'error',
     'vue-pug-sfc/require-component-is': 'error',
     'vue-pug-sfc/require-v-for-key': 'error',
-    'vue-pug-sfc/use-v-on-exact': 'error'
+    'vue-pug-sfc/use-v-on-exact': 'error',
+    'vue-pug-sfc/valid-v-if': 'error'
   }
 };
