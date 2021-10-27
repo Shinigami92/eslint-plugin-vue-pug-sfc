207fdf9d83a46568f26160fdea3e09a708db9254
Shinigami92 <chrissi92@hotmail.de>
2021-10-27 17:07:26 +0200

    Update project

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,5 +1,11 @@
+// @ts-check
+
+/**
+ * @type {import('prettier').Options}
+ */
 module.exports = {
   plugins: [require.resolve('prettier-plugin-organize-imports')],
+
   arrowParens: 'always',
   bracketSpacing: true,
   printWidth: 120,
