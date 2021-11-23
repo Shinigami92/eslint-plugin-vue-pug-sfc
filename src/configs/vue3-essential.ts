b21a98caf53ab415913b1421c86c1fc9db750d74
Christopher Quadflieg <christopher.quadflieg@adsoul.com>
2021-11-23 16:05:26 +0100

    Not relevant for Vue 3

--- src/configs/vue3-essential.ts
+++ src/configs/vue3-essential.ts
@@ -5,7 +5,6 @@ export = {
     'vue-pug-sfc/no-deprecated-filter': 'error',
     'vue-pug-sfc/no-dupe-v-else-if': 'error',
     'vue-pug-sfc/no-duplicate-attributes': 'error',
-    'vue-pug-sfc/no-multiple-template-root': 'error',
     'vue-pug-sfc/no-template-key': 'error',
     'vue-pug-sfc/no-textarea-mustache': 'error',
     'vue-pug-sfc/no-use-v-if-with-v-for': 'error',
