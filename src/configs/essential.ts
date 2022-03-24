c96bf137873ac2774ba5f4642d91f69147b79b7f
rash <rash@rash.codes>
2022-03-24 18:55:14 +0100

    valid-v-{else-if,else,for,if} working out of the box

--- src/configs/essential.ts
+++ src/configs/essential.ts
@@ -1,9 +1,4 @@
 export = {
   extends: require.resolve('./base'),
-  rules: {
-    'vue-pug-sfc/valid-v-else-if': 'error',
-    'vue-pug-sfc/valid-v-else': 'error',
-    'vue-pug-sfc/valid-v-for': 'error',
-    'vue-pug-sfc/valid-v-if': 'error',
-  },
+  rules: {},
 };
--- src/rules/index.ts
+++ src/rules/index.ts
@@ -4,10 +4,6 @@ import componentNameInTemplateCasing from './component-name-in-template-casing';
 import noDeprecatedDollarScopedslotsApi from './no-deprecated-dollar-scopedslots-api';
 import noDeprecatedFilter from './no-deprecated-filter';
 import thisInTemplate from './this-in-template';
-import validVElse from './valid-v-else';
-import validVElseIf from './valid-v-else-if';
-import validVFor from './valid-v-for';
-import validVIf from './valid-v-if';
 
 export default {
   'attribute-hyphenation': attributeHyphenation,
@@ -15,8 +11,4 @@ export default {
   'no-deprecated-dollar-scopedslots-api': noDeprecatedDollarScopedslotsApi,
   'no-deprecated-filter': noDeprecatedFilter,
   'this-in-template': thisInTemplate,
-  'valid-v-else-if': validVElseIf,
-  'valid-v-else': validVElse,
-  'valid-v-for': validVFor,
-  'valid-v-if': validVIf,
 } as Record<string, Rule.RuleModule>;
