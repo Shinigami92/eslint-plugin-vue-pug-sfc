add032bbd729997e6fb696fa3377e3cdf2c882e1
rash <rash@rash.codes>
2022-03-26 20:01:28 +0100

    no-deprecated-dollar-scopedslots-api works out of the box

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,14 +1,12 @@
 import type { Rule } from 'eslint';
 import attributeHyphenation from './attribute-hyphenation';
 import componentNameInTemplateCasing from './component-name-in-template-casing';
-import noDeprecatedDollarScopedslotsApi from './no-deprecated-dollar-scopedslots-api';
 import noDeprecatedFilter from './no-deprecated-filter';
 import thisInTemplate from './this-in-template';
 
 export default {
   'attribute-hyphenation': attributeHyphenation,
   'component-name-in-template-casing': componentNameInTemplateCasing,
-  'no-deprecated-dollar-scopedslots-api': noDeprecatedDollarScopedslotsApi,
   'no-deprecated-filter': noDeprecatedFilter,
   'this-in-template': thisInTemplate,
 } as Record<string, Rule.RuleModule>;
